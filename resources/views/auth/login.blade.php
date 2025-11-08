628b240d22aa1796cee364dc2b5e36d1db60c799
unknown <rinku696915@gmail.com>
2025-11-08 13:28:42 +0700

    add ui login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,33 +3,31 @@
 @section('title', 'Login')
 
 @section('content')
-<div class="row justify-content-center mt-5">
-    <div class="col-md-6">
+    <div class="wrapper col-md-5">
         <div class="card">
             <div class="card-header">Login</div>
             <div class="card-body">
                 <form method="POST" action="{{ route('login.post') }}">
                     @csrf
-
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autofocus>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                            name="email" value="{{ old('email') }}" required autofocus>
                         @error('email')<div class="invalid-feedback">{{ $message }}</div>@enderror
                     </div>
 
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                            name="password" required>
                         @error('password')<div class="invalid-feedback">{{ $message }}</div>@enderror
                     </div>
 
-                    <div class="d-flex justify-content-between align-items-center">
-                        <a href="{{ route('register.show') }}">Belum punya akun? Daftar</a>
+                    <div class="wrapper-login d-flex justify-content-center align-items-center py-2 pt-3">
                         <button type="submit" class="btn btn-primary">Login</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
-</div>
 @endsection
