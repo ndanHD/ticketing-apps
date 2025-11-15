27f7508ce3abbcd486b09634c81a92d822eabc6f
alamul huda <hudaalamul23@gmail.com>
2025-11-15 16:01:19 +0700

    delete migraton

--- database/migrations/2025_11_15_045223_create_notification_settings_table.php
+++ database/migrations/2025_11_15_045223_create_notification_settings_table.php
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('notification_settings');
+        Schema::dropIfExists('tbl_notification_settings');
     }
 };
